d66bafdec5b0e4785e1c2f6360ad0ea15382c0c4
xiyu <1365755570@qq.com>
2023-03-21 13:57:13 +0800

    Vue-CLI(运行流程分析)

---
+++ vue-cli-test/src/components/HelloBaizhi.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>hello {{content}}</p>
+</template>
+
+<script>
+  export default {
+    name: 'HelloBaizhi',
+    data() {
+      return {
+        content: "baizhi"
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+    color: yellow;
+  }
+</style>
